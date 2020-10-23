13eebcda9b2174392356ea4d922e82435a363bf2
Alicia Weenum <weenum.alicia@gmail.com>
2020-10-22 23:03:37 -0400

    commit tree 2c2fc9d0f8e914e02627c7955c9067b8833da141

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,17 +2,20 @@
   # array.join
 #end
 
+   
+
 def oxford_comma(array)
-    if array.count == 1
-return array.join
-    elsif array.count ==2
-        array.join(" and ")
-    else
-        string = array[0..-2].join(", ")
-        string << ", and"
-        string << " #{array[-1]}"
-    end
-end
+    if array.size >= 3 
+      last = array.pop
+      array.push('and '+last)
+      array.join(', ')
+    elsif array.size >= 1
+      array.join(' and ')  
+   # else
+     # array.join  
+   end  
+  end
+  
 
 #adds commas plus a final 'and' when given a 3-element array (FAILED - 1)
 # correctly formats arrays of lengths greater than three (FAILED - 2)
